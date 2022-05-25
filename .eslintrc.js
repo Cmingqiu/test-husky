244189e49ee1fc73d62956874b335c272e675e15
chenmq <1075124372@qq.com>
2022-05-25 21:32:36 +0800

    feat: 新增pre-commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  rules: {
+    'no-undef': 2,
+    'semi': [2, 'never'],
+    "no-debugger": 2
+  }
+}
